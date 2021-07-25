104029e93bcc7105cf5cd5bdfb58ac7d4c6d87d6
suryar0506 <72110042+suryar0506@users.noreply.github.com>
2021-07-25 12:32:25 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var img = "";
+var status = "";
 
 function preload(){
 img = loadImage("dog_cat.jpg");
@@ -7,10 +8,27 @@ img = loadImage("dog_cat.jpg");
 function setup(){
     canvas = createCanvas(640, 420);
     canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status_display").innerHTML = "Detecting objects ";
+}
+
+function modelLoaded(){
+    console.log("Model is ready to go!");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, result){
+    if(error){
+        console.log(error);
+    } else {
+        console.log(result);
+    }
 }
 
 function draw(){
     image(img, 0, 0, 640, 420);
+
     fill('red');
     textSize(20);
     textFont('Arial');
@@ -19,5 +37,15 @@ function draw(){
     noFill();
     strokeWeight(5);
     stroke('red');
-    rect(30, 60, 350, 450);
+    rect(30, 60, 700, 450);
+
+    fill('red');
+    textSize(20);
+    textFont('Arial');
+    strokeWeight(2);
+    text("Cat", 275, 90);
+    noFill();
+    strokeWeight(5);
+    stroke('red');
+    rect(260, 60, 350, 450);
 }
