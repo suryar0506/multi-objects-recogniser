92803cdfe2999f2ff5ac6f43268fe254adaab4ab
suryar0506 <72110042+suryar0506@users.noreply.github.com>
2021-07-28 17:27:16 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 var img = "";
 var status = "";
+var objects = [];
 
 function preload(){
 img = loadImage("dog_cat.jpg");
 }
 
 function setup(){
-    canvas = createCanvas(640, 420);
+    canvas = createCanvas(700, 500);
     canvas.center();
     objectDetector = ml5.objectDetector('cocossd', modelLoaded);
     document.getElementById("status_display").innerHTML = "Detecting objects ";
@@ -23,29 +24,26 @@ function gotResult(error, result){
         console.log(error);
     } else {
         console.log(result);
+        objects = result;
     }
 }
 
 function draw(){
-    image(img, 0, 0, 640, 420);
-
-    fill('red');
-    textSize(20);
-    textFont('Arial');
-    strokeWeight(2);
-    text("Dog", 45, 90);
-    noFill();
-    strokeWeight(5);
-    stroke('red');
-    rect(30, 60, 700, 450);
-
-    fill('red');
-    textSize(20);
-    textFont('Arial');
-    strokeWeight(2);
-    text("Cat", 275, 90);
-    noFill();
-    strokeWeight(5);
-    stroke('red');
-    rect(260, 60, 350, 450);
+    image(img, 0, 0, 700, 500);
+    
+if(status != ""){
+    for(var i = 0; i<objects.length; i++){
+        fill('red');
+        textSize(15);
+        textFont('Arial');
+        strokeWeight(2);
+        percentage = floor(objects[i].confidence * 100);
+        text(objects[i].label + " " + percentage + "%", objects[i].x + 10, objects[i].y + 30);
+        noFill();
+        strokeWeight(5);
+        stroke('red');
+        rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+    }
+}
+    
 }
